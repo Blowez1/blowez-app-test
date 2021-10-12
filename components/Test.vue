3006be60444d6d58e33f6f03f43e30b1fbe26a51
Berke Batur <aberkebatur@gmail.com>
2021-10-12 17:39:25 +0300

    first commit

---
+++ components/Test.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul>
+        <h2>Schools</h2>
+        <li v-for="school in schools" :key="school.name">
+            <a :href="school.web_pages[0]" target="_BLANK">{{school.name}}</a>
+        </li>
+    </ul>
+</template>
+<script>
+export default {
+    props : {
+        schools : Array
+    }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Test :schools="schools" />
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      schools: null,
+    };
+  },
+  async fetch() {
+    this.schools = await this.$axios.$get(
+      "http://universities.hipolabs.com/search?country=Turkey"
+    );
+  },
+};
+</script>
